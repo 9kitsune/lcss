5f6db4f633e3c3210444b7bf99748b608163710d
9kitsune <nine.kitsune@gmail.com>
2019-05-04 20:27:14 +0100

    added to github

--- logger.py
+++ logger.py
@@ -1,32 +1,37 @@
-from sense_hat import SenseHat 
+from lsm303d import LSM303D
 from datetime import datetime
 from time import sleep
 from csv import writer
 
-sense = SenseHat()
-winks = 30  #rest 30 seconds before sampling
+lsm = LSM303D(0x1d)
+sample_rate = 0.1  #rest 0.1 seconds before taking a sample
+duration = 10 # in 10 seconds period
+count = duration/sample_rate # number of samples in the time period
+counter = 1
 
 def get_acc():
 	acc = sense.get_accelerometer_raw()
 	return acc
+# xyz = lsm.accelerometer()
+
 
 def get_sense_data():
 	sense_data = []
-	x,y,z = [sense.get_accelerometer_raw()[key] for key in ['x','y','z']]
+	xyz = lsm.accelerometer()
+	x,y,z = [xyz[key] for key in [0,1,2]]
 	sense_data.append(x)
 	sense_data.append(y)
 	sense_data.append(z)
-	yaw,pitch,roll = [sense.get_orientation()[key] for key in ['yaw','pitch','roll']]
-    sense_data.append(pitch)
-	sense_data.append(roll)
-	sense_data.append(yaw)
 	sense_data.append(datetime.now())
 	return sense_data
 
 with open('baseline.csv', 'w') as f:
-    data_writer = writer(f)
-    data_writer.writerow(['x','y','z','pitch','roll','yaw', 'datetime'])
-    while True:
-        data = get_sense_data()
-        data_writer.writerow(data)
-        sleep(winks)
+	data_writer = writer(f)
+	data_writer.writerow(['x','y','z','datetime'])
+	while counter < count:
+		data = get_sense_data()
+		data_writer.writerow(data)
+		sleep(sample_rate)
+		counter += 1
+		if counter > count:
+			break
